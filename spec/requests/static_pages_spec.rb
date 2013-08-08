4d18fda30ddef8cbd788c8d8c3a31bd1895876a3
faz-ilnaz <ilnaz94@gmail.com>
2013-08-08 18:41:52 +0400

    Finish users logic

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject {page}
 
   describe "Home page" do
-
-    it "should have the content 'Guest book'" do
-      visit '/static_pages/home'
-      page.should have_content('Guest book')
-    end
+    before { visit root_path }
+    it { should have_selector('title' , text: 'Guest book') }
 
     # it "should have the title 'Home'" do
     #   visit '/static_pages/home'
@@ -15,5 +13,12 @@ describe "Static pages" do
     #                     :text => "Guest book | Home")
     # end
 
+  end
+
+  describe "signup page" do
+    before { visit new_user_registration_path }
+
+    it { should have_selector('h2', text: 'Sign up') }
+    it { should have_selector('title', text: 'Sign up') }
   end
 end
